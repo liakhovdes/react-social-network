60482821f8773d6f673cfc794fff700980b07fe7
Denys L <liakhovdes@gmail.com>
2020-05-03 14:00:15 +0200

    first commit

---
+++ src/componetns/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://lh3.googleusercontent.com/proxy/deCmxmU0D1jRT6KRkU9ZS0YMIPIVi9F0QuFAl_ga15NlkcdyhnTURa11JaQG8VOLlkz3IV_mjjpl32xYOHt696jpv1j2cftBIMevE6QPoSzDIIIEx-ddug' />
+    </header>
+}
+
+export default Header;
---
+++ src/componetns/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80' />
+        </div>
+        <div>
+            ava + description
+    </div>
+        <div>
+            My posts
+      <div>
+                New post
+      </div>
+            <div>
+                post 1
+      </div>
+            <div>
+                post 2
+      </div>
+        </div>
+    </div>
+}
+
+export default Profile;
