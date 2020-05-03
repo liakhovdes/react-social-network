9cbfcb42d1ca9ef03ac183fef7e41599c3ffcf81
Denys L <liakhovdes@gmail.com>
2020-05-03 15:25:07 +0200

    14 css-modules

--- src/componetns/Header.jsx
+++ src/componetns/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src='https://lh3.googleusercontent.com/proxy/deCmxmU0D1jRT6KRkU9ZS0YMIPIVi9F0QuFAl_ga15NlkcdyhnTURa11JaQG8VOLlkz3IV_mjjpl32xYOHt696jpv1j2cftBIMevE6QPoSzDIIIEx-ddug' />
     </header>
 }
-
 
 export default Header;
--- src/componetns/Profile.jsx
+++ src/componetns/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from'./Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
             <img src='https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80' />
         </div>
@@ -10,18 +11,19 @@ const Profile = () => {
     </div>
         <div>
             My posts
-      <div>
-                New post
-      </div>
-            <div>
-                post 1
-      </div>
-            <div>
-                post 2
-      </div>
+                <div>
+                    New post
+                </div>
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        post 1
+                    </div>
+                    <div className={s.item}>
+                        post 2
+                    </div>
+                </div>
         </div>
     </div>
 }
-
 
 export default Profile;
