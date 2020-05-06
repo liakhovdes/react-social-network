ddb480db733ccc4e562a07c671fec3d22be0591c
Denys L <liakhovdes@gmail.com>
2020-05-06 22:45:44 +0200

    19-20 Route and NavLink

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@ import Header from './componetns/Header/Header';
 import Navbar from './componetns/Navbar/Navbar';
 import Profile from './componetns/Profile/Profile';
 import Dialogs from "./componetns/Dialogs/Dialogs";
+import News from "./componetns/News/News";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Music from "./componetns/Music/Music";
+import Settings from "./componetns/Settings/Settings";
 
-const App = () => {
+
+
+const App = (props) => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs />
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/news' component={News} />
+                    <Route path='/music' component={Music} />
+                    <Route path='/settings' component={Settings} />
+                </div>
             </div>
-            {/*<Profile />*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
